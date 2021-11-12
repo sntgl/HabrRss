d2a726579116834f5d5abb391ed6e46f53b066e1
Aleksandr Tagilov <saniatagilov@gmail.com>
2021-11-12 04:04:57 +0300

    chips complete

--- app/src/main/java/com/example/habrrss/FeedViewHolder.kt
+++ app/src/main/java/com/example/habrrss/FeedViewHolder.kt
@@ -6,19 +6,34 @@ import com.example.habrrss.databinding.FeedItemBinding
 import android.text.Editable
 import org.xml.sax.XMLReader
 import android.text.method.LinkMovementMethod
-import android.text.method.TimeKeyListener
-import timber.log.Timber
+import androidx.core.content.ContextCompat
+import com.google.android.material.chip.Chip
 
 class FeedViewHolder(
     private val binding: FeedItemBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: FeedItem) {
+            bindRichText(item.description)
+            bindChips(item.categories)
+            bindSimpleViews(item)
+    }
+
+    private fun bindSimpleViews(item: FeedItem) {
+        with(binding) {
+            itemTitle.text = item.title
+            itemAuthor.text = item.creator
+            itemDatePublished.text = item.date
+        }
+    }
+
+    private fun bindRichText(description_nullable: String?) {
+        val description = description_nullable ?: ""
         with(binding) {
             itemDescription.movementMethod = LinkMovementMethod.getInstance()
             itemDescription.text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
                 Html.fromHtml(
-                    item.description,
+                    description,
                     Html.FROM_HTML_MODE_COMPACT,
                     URLImageParser(itemDescription),
                     { _: Boolean, _: String, _: Editable, _: XMLReader ->
@@ -26,11 +41,29 @@ class FeedViewHolder(
                     }
                 )
             else
-                Html.fromHtml(item.description)
-            itemTitle.text = item.title
-            itemAuthor.text = item.creator
-            itemDatePublished.text = item.date
-            Timber.d("${item.categories?.size}")
+                Html.fromHtml(description)
+        }
+    }
+
+    private fun bindChips(
+        categories: List<Category>?
+    ) {
+        with(binding) {
+            itemChipContainer.removeAllViews()
+            if (categories != null && categories.isNotEmpty()) {
+                categories.forEach {
+                    val chip = Chip(root.context)
+                    chip.text = it.name
+                    chip.setChipBackgroundColorResource(R.color.design_default_color_primary)
+                    chip.setTextColor(
+                        ContextCompat.getColor(
+                            root.context,
+                            R.color.design_default_color_on_primary
+                        )
+                    )
+                    itemChipContainer.addView(chip)
+                }
+            }
         }
     }
 }
